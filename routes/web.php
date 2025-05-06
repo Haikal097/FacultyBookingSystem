9407a70336ba7f85ace2f702dcead596fc235fa8
Afiq Haikal <afiqh101@gmail.com>
2025-05-07 03:56:58 +0800

    Added Infographic & Profile pages

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/reservation', function () {
-    return view('reservation');
-});
+Route::get('/userprofile', function () {
+    return view('userprofile');
+})->name('userprofile'); 
+
+Route::get('/infographic', function () {
+    return view('infographic');
+})->name('infographic'); 
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -19,6 +23,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+use App\Http\Controllers\BookingController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/booking', [BookingController::class, 'create'])->name('bookings.create');
+    Route::post('/booking', [BookingController::class, 'store'])->name('bookings.store');
 });
 
 require __DIR__.'/auth.php';
