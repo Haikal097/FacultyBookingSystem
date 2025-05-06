d91386b0fc494c53fc4e2c77a9ceb3b24a857466
Afiq Haikal <afiqh101@gmail.com>
2025-05-07 04:08:52 +0800

    Added IC Number & Phone Number to Users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'ic_number',
+        'phone_number',
     ];
 
     /**
---
+++ database/migrations/2025_05_06_200615_add_ic_and_phone_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('ic_number')->nullable()->after('email'); // Adjust 'after' as needed
+            $table->string('phone_number')->nullable()->after('ic_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['ic_number', 'phone_number']);
+        });
+    }
+};
